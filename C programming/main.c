ba65541dcbf097c4d32f62de5ed7e379b0a69c46
lilyandcy <myyan_yan@msn.com>
2018-08-19 10:20:51 +0800

    Initial Commit

---
+++ C programming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C programming
+//
+//  Created by yanche2 on 2018/8/19.
+//  Copyright © 2018年 lilyandcy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
